1b8422cb7e9c953f26c9c83d8c6fb6853ad5eed7
Nyholm <tobias.nyholm@gmail.com>
2020-01-29 23:35:08 +0100

    Added more boilerplate code

--- SendMessageResult.php
+++ SendMessageResult.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace WorkingTitle\Aws\Sqs;
 
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
 class SendMessageResult
 {
-    public function __construct(string $body)
+    public function __construct(ResponseInterface $response)
     {
         // Do something..
     }
